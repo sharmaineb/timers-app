9eb34a76da92142a43cca9dfe31c425445072cd5
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-03-31 22:03:51 -0700

    minor electron.js update

--- public/electron.js
+++ public/electron.js
@@ -6,8 +6,8 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 900,
-    height: 680,
+    width: 1000,
+    height: 750,
     webPreferences: {
       nodeIntegration: true
     }
